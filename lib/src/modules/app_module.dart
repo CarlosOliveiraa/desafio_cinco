fa398ecc1ecb2c2d37dd05643b78e05fd874c596
Carlos Oliveira <carloshenrique.o.e@gmail.com>
2022-02-25 16:12:04 -0300

    Iniciando a implementação do modular

---
+++ lib/src/modules/app_module.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+
+  @override
+  List<Bind> get binds => [];
+
+
+  @override
+  List<ModularRoute> get routes => [
+    
+  ];
+
+
+}
